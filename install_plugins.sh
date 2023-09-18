8c3d229af2e6c3035484719175b0f0272dbb6bd3
William Guimont-Martin <wil9876543210@hotmail.com>
2023-09-18 00:47:17 -0400

    more plugins

--- install_plugins.sh
+++ install_plugins.sh
@@ -10,10 +10,12 @@ git clone https://github.com/Stability-AI/stability-ComfyUI-nodes
 git clone https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes
 git clone https://github.com/EllangoK/ComfyUI-post-processing-nodes
 git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+git clone https://github.com/LucianoCirino/efficiency-nodes-comfyui
 
 pip install -r ComfyUI-Manager/requirements.txt
 pip install -r comfyui_controlnet_aux/requirements.txt
 pip install -r stability-ComfyUI-nodes/requirements.txt
+pip install -r efficiency-nodes-comfyui/requirements.txt
 
 # Copy models
 cp -r /app/output/Models/* /app/StableSwarmUI/Models
